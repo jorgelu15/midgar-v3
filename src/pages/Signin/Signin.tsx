6dbadf5f067af6ca312245d85c2ad93b83b58ea8
Jorge Guardo <j.guardo.ro@gmail.com>
2025-08-10 15:50:19 -0500

    cambios en las alertas del sign in

--- src/pages/Signin/Signin.tsx
+++ src/pages/Signin/Signin.tsx
@@ -8,8 +8,10 @@ import { routes } from '../../utils/routes';
 import { useForm } from '../../hooks/useForm';
 import { useEffect, useState } from 'react';
 import { useAuth } from '../../hooks/useAuth';
+import { useTheme } from '../../context/ThemeContext/ThemeContext';
 const Signin = () => {
     const { isAuthenticated, signIn } = useAuth();
+    const { theme } = useTheme();
 
     const { form, onChangeGeneral } = useForm({
         email: "",
@@ -99,7 +101,7 @@ const Signin = () => {
                 pauseOnFocusLoss
                 draggable
                 pauseOnHover
-                theme="light"
+                theme={theme}
                 transition={Bounce}
             />
         </div>
