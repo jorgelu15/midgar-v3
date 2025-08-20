ba3d2b262e6f79428db62fb6a3eb3ec156c498b9
Jorge Guardo <j.guardo.ro@gmail.com>
2025-08-20 15:26:58 -0500

    cambio de url

--- src/config/axios.tsx
+++ src/config/axios.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const clienteAxios = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL,
+    baseURL: import.meta.env.VITE_BASE_URL ?? "https://api-midgarsa.artosh.dev/api/v1",
     withCredentials: true
 });
 
