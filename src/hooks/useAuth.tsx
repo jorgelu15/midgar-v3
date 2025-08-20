a7ae6b23968749774fdf75f44a67e4b54a8409d6
Jorge Guardo <j.guardo.ro@gmail.com>
2025-08-20 15:01:53 -0500

    correccion de nombre de carpeta para deploy

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,9 +1,9 @@
 import { useContext } from "react"
-import authContext from "../context/Authentication/authContext";
+import authContext from "../context/authentication/authContext";
 
 export const useAuth = () => {
     const { isAuthenticated, usuario, msg, cargando, signIn, signUp, enableMFA, verifyMFA, usuarioAutenticado, logOut }: any = useContext(authContext);
-
+rev
     return {
         isAuthenticated,
         usuario,
